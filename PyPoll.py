43137d8109eb142b43fd003ebfaef6d1e763021e
kajev <kajevm@gmail.com>
2021-11-21 16:27:42 -0500

    Adding PyPoll.py file

---
+++ PyPoll.py
@@ -0,0 +1,24 @@
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of thhe election based on popular vote 
+
+# assign a variable for the file to load and the path
+
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+with open(file_to_load) as election_data:
+
+
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
+
