75c50e20c87575ae7a264365175ad9bfed6b2e3f
kajev <kajevm@gmail.com>
2021-11-21 19:13:11 -0500

    Adding the election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -21,6 +21,7 @@ candidate_options = []
 
 candidate_votes = {}
 
+winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 # Using the open() function with the "w" mode we will write data to the file.
@@ -41,14 +42,29 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 1
         else:
             candidate_votes[candidate_name] += 1
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
     for candidate_name in candidate_votes:
         #retrieve the vote count of a candidate
         votes = candidate_votes[candidate_name] 
         #calculate the percentage of votes
         vote_percentage = round(float(votes) / float(total_votes) * 100, 2)
         #print the candidate name and percentage of votes
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
         #print(f" {candidate_name}: received {vote_percentage}% of the vote")
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        
+        
+            
+        print(candidate_results)
+        txt_file.write(candidate_results)  
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
             winning_percentage = vote_percentage
@@ -61,5 +77,5 @@ with open(file_to_load) as election_data:
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
-    print(winning_candidate_summary)
+    #print(winning_candidate_summary)
 
